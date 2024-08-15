4291318288e1e58b7c44da0b2d7dc388458090a9
Rishan-Jadva <67826633+Rishan-Jadva@users.noreply.github.com>
2024-08-15 13:07:22 +1000

    Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0){
+        return "ERROR"
+    }
+    sum = 0;
+    for (let i = Math.max(a,b); i >= Math.min(a,b); i--){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
