08b14c5ea32fa6dec2191c6832d99e1eee2b615b
Rishan-Jadva <67826633+Rishan-Jadva@users.noreply.github.com>
2024-08-18 18:10:53 +1000

    12_findTheOldest completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+        const currentYear = new Date().getFullYear();
+        
+        const oldestPerson = people.reduce((oldest, person) => {
+            const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+            const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+            
+            return personAge > oldestAge ? person : oldest;
+        });
+        return oldestPerson;    
 };
 
 // Do not edit below this line
