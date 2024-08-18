5dcbba799967817db8c18b5c2732b4aad2634576
Rishan-Jadva <67826633+Rishan-Jadva@users.noreply.github.com>
2024-08-18 17:53:24 +1000

    10_fibonacci.js completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0){
+        return "OOPS"
+    } else if (n === 0){
+        return 0;
+    }
+    let prev = 0;
+    let curr = 1;
+    let temp;
+    for (let i = 1; i < n; i++){
+        temp = prev;
+        prev = curr;
+        curr += temp;
+    }
+    return curr
 };
 
 // Do not edit below this line
